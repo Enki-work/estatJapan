b120d319474cef0617c05c48a63029af88291874
Enki <wdydhd@vip.qq.com>
2021-08-29 10:16:41 +0900

    dio　テスト

--- estatjapan/lib/main.dart
+++ estatjapan/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,20 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  static const appId = '7bed85b352e6c3d46ad6def4390196b23d86bcec';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -61,12 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Dio _dio = Dio();
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -93,8 +81,21 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FutureBuilder(
+              future: _dio.get(
+                  "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=7bed85b352e6c3d46ad6def4390196b23d86bcec&lang=J&statsDataId=0003423913&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (false) {
+                  Response response = snapshot.data;
+                  return ListView(
+                      children: response.data
+                          .map<Widget>((e) => ListTile(title: Text(e["aa"]))));
+                } else {
+                  return Text(
+                    '$snapshot Error \n You have pushed the button this many times:',
+                  );
+                }
+              },
             ),
             Text(
               '$_counter',
