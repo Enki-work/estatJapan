f807f1f5f6313b50c8a02d6d2318b08528337752
Enki <wdydhd@vip.qq.com>
2021-08-30 08:56:46 +0900

    JSON解析不具合修正

--- estatjapan/lib/model/StatsData.g.dart
+++ estatjapan/lib/model/StatsData.g.dart
@@ -9,9 +9,12 @@ part of 'StatsData.dart';
 StatsData _$StatsDataFromJson(Map<String, dynamic> json) {
   return StatsData(
     RESULT: Result.fromJson(json['RESULT'] as Map<String, dynamic>),
+    STATISTICAL_DATA: StatisticalData.fromJson(
+        json['STATISTICAL_DATA'] as Map<String, dynamic>),
   );
 }
 
 Map<String, dynamic> _$StatsDataToJson(StatsData instance) => <String, dynamic>{
       'RESULT': instance.RESULT,
+      'STATISTICAL_DATA': instance.STATISTICAL_DATA,
     };
