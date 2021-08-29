66bfdf20f4bf73d6659b01a4b2d43430654ea14b
Enki <wdydhd@vip.qq.com>
2021-08-29 19:17:21 +0900

    ボトムバー実装

--- estatjapan/lib/Page/ScaffoldRoute.dart
+++ estatjapan/lib/Page/ScaffoldRoute.dart
@@ -1,21 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:estatjapan/model/ImmigrationStatisticsRoot.dart';
 import 'package:flutter/material.dart';
 
 import 'MenuDrawer.dart';
 
 class ScaffoldRoute extends StatefulWidget {
+  final String title;
+
+  const ScaffoldRoute({Key? key, required this.title}) : super(key: key);
   @override
   _ScaffoldRouteState createState() => _ScaffoldRouteState();
 }
 
 class _ScaffoldRouteState extends State<ScaffoldRoute> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Dio _dio = Dio();
     return Scaffold(
       appBar: AppBar(
         //导航栏
-        title: Text("App Name"),
+        title: Text(widget.title),
         actions: <Widget>[
           //导航栏右侧菜单
           IconButton(icon: Icon(Icons.share), onPressed: () {}),
@@ -35,10 +41,58 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
         fixedColor: Colors.blue,
         onTap: _onItemTapped,
       ),
-      floatingActionButton: FloatingActionButton(
-          //悬浮按钮
-          child: Icon(Icons.add),
-          onPressed: _onAdd),
+      body: (() {
+        switch (_selectedIndex) {
+          case 0:
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      alignment: Alignment.center,
+                      child: FutureBuilder(
+                        future: _dio.get(
+                            "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=7bed85b352e6c3d46ad6def4390196b23d86bcec&lang=J&statsDataId=0003423913&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"),
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+//请求完成
+                            if (snapshot.hasError) {
+//发生错误
+                              return Text(snapshot.error.toString());
+                            }
+                            if (false) {
+// Response response = snapshot.data;
+// return ListView(
+//     children: response.data.map<Widget>(
+//         (e) => ListTile(title: Text(e["aa"]))));
+                            } else {
+                              Response response = snapshot.data;
+                              ImmigrationStatisticsRoot rootModel =
+                                  ImmigrationStatisticsRoot.fromJson(
+                                      response.data);
+                              String a =
+                                  rootModel.GET_STATS_DATA.RESULT.ERROR_MSG;
+                              return Text(
+                                '$a',
+                              );
+                            }
+                          } else {
+//请求未完成时弹出loading
+                            return CircularProgressIndicator();
+                          }
+                        },
+                      )),
+                ],
+              ),
+            );
+          case 1:
+            return Center(child: Text("111"));
+          case 2:
+            return Center(child: Text("222"));
+        }
+      })(),
     );
   }
 
@@ -47,6 +101,4 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
       _selectedIndex = index;
     });
   }
-
-  void _onAdd() {}
 }
--- estatjapan/lib/main.dart
+++ estatjapan/lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:dio/dio.dart';
+import 'package:estatjapan/Page/ScaffoldRoute.dart';
 import 'package:flutter/material.dart';
-
-import 'Page/MenuDrawer.dart';
-import 'model/ImmigrationStatisticsRoot.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,85 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Dio _dio = Dio();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: new MenuDrawer(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                child: FutureBuilder(
-                  future: _dio.get(
-                      "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=7bed85b352e6c3d46ad6def4390196b23d86bcec&lang=J&statsDataId=0003423913&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      //请求完成
-                      if (snapshot.hasError) {
-                        //发生错误
-                        return Text(snapshot.error.toString());
-                      }
-                      if (false) {
-                        // Response response = snapshot.data;
-                        // return ListView(
-                        //     children: response.data.map<Widget>(
-                        //         (e) => ListTile(title: Text(e["aa"]))));
-                      } else {
-                        Response response = snapshot.data;
-                        ImmigrationStatisticsRoot rootModel =
-                            ImmigrationStatisticsRoot.fromJson(response.data);
-                        String a = rootModel.GET_STATS_DATA.RESULT.ERROR_MSG;
-                        return Text(
-                          '$a \n You have pushed the button this many times:',
-                        );
-                      }
-                    } else {
-                      //请求未完成时弹出loading
-                      return CircularProgressIndicator();
-                    }
-                  },
-                )),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: ScaffoldRoute(title: 'Flutter Demo Home Page'),
     );
   }
 }
