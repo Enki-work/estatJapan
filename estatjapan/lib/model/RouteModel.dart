f07297909164397c49efe8f71ab8a0ea1e2a54e1
Enki <wdydhd@vip.qq.com>
2021-08-30 15:49:44 +0900

    Route Model作成

---
+++ estatjapan/lib/model/RouteModel.dart
@@ -0,0 +1,8 @@
+import 'Class.dart';
+import 'ImmigrationStatisticsRoot.dart';
+
+class RouteModel {
+  Class selectedCLASS;
+  ImmigrationStatisticsRoot? rootModel;
+  RouteModel({required this.selectedCLASS, this.rootModel});
+}
