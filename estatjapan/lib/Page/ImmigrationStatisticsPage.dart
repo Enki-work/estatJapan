7f1358a8cf0fdd0c8b5cb2ce783a2621e41a542b
Enki <wdydhd@vip.qq.com>
2021-08-30 13:00:29 +0900

    Page ModelはProviderでリファクタリング

--- estatjapan/lib/Page/ImmigrationStatisticsPage.dart
+++ estatjapan/lib/Page/ImmigrationStatisticsPage.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:estatjapan/model/Class.dart';
+import 'package:estatjapan/model/ImmigrationStatisticsModel.dart';
 import 'package:estatjapan/model/ImmigrationStatisticsRoot.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'MenuDrawer.dart';
 
@@ -17,69 +19,86 @@ class ImmigrationStatisticsPage extends StatefulWidget {
 }
 
 class _ImmigrationStatisticsPageState extends State<ImmigrationStatisticsPage> {
-  int _selectedIndex = 0;
+  ImmigrationStatisticsModel model = ImmigrationStatisticsModel();
 
   @override
   Widget build(BuildContext context) {
     Dio _dio = Dio();
-    return Scaffold(
-        appBar: AppBar(
-          //导航栏
-          title: Text(widget.title),
-          actions: <Widget>[
-            //导航栏右侧菜单
-            // IconButton(icon: Icon(Icons.share), onPressed: () {}),
-          ],
-        ),
-        drawer: new MenuDrawer(), //抽屉
-        bottomNavigationBar: BottomNavigationBar(
-          // 底部导航
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.add_chart_rounded), title: Text('在留資格審査')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.align_horizontal_left_rounded),
-                title: Text('審査受理・処理')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.all_inbox_rounded), title: Text('在留管理局・支局')),
-          ],
-          currentIndex: _selectedIndex,
-          fixedColor: Colors.blue,
-          onTap: _onItemTapped,
-        ),
-        body: Container(
-          child: FutureBuilder(
-              future: _dio.get(
-                  "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=7bed85b352e6c3d46ad6def4390196b23d86bcec&lang=J&statsDataId=0003423913&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasError) {
-                    return Text(snapshot.error.toString());
-                  }
-                  Response response = snapshot.data;
-                  ImmigrationStatisticsRoot rootModel =
-                      ImmigrationStatisticsRoot.fromJson(response.data);
-                  switch (_selectedIndex) {
-                    case 0:
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          _cat01ListView(rootModel),
-                        ],
-                      );
-                    case 1:
-                      return Center(child: Text("111"));
-                    case 2:
-                      return Center(child: Text("222"));
-                    default:
-                      return Center(child: Text("予想外エラー"));
-                  }
-                } else {
+    return FutureBuilder(
+        future: _dio.get(
+            "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=7bed85b352e6c3d46ad6def4390196b23d86bcec&lang=J&statsDataId=0003423913&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            Response response = snapshot.data;
+            ImmigrationStatisticsRoot rootModel =
+                ImmigrationStatisticsRoot.fromJson(response.data);
+            model.rootModel = rootModel;
+            return Scaffold(
+                appBar: AppBar(
+                  //导航栏
+                  title: Text(widget.title),
+                  actions: <Widget>[
+                    //导航栏右侧菜单
+                    // IconButton(icon: Icon(Icons.share), onPressed: () {}),
+                  ],
+                ),
+                drawer: new MenuDrawer(), //抽屉
+                bottomNavigationBar: BottomNavigationBar(
+                  // 底部导航
+                  items: <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.add_chart_rounded),
+                        title: Text('在留資格審査')),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.align_horizontal_left_rounded),
+                        title: Text('審査受理・処理')),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.all_inbox_rounded),
+                        title: Text('在留管理局・支局')),
+                  ],
+                  currentIndex: model.selectedIndex,
+                  fixedColor: Colors.blue,
+                  onTap: _onItemTapped,
+                ),
+                body: ChangeNotifierProvider<ImmigrationStatisticsModel>(
+                    create: (_) => model,
+                    child: Builder(builder: (context) {
+                      switch (Provider.of<ImmigrationStatisticsModel>(context)
+                          .selectedIndex) {
+                        case 0:
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Consumer<ImmigrationStatisticsModel>(
+                                builder: (context, value, _) {
+                                  if (value.model == null)
+                                    return Center(child: Text("予想外エラー"));
+                                  return _cat01ListView(value.model!);
+                                },
+                              )
+                            ],
+                          );
+                        case 1:
+                          return Center(child: Text("111"));
+                        case 2:
+                          return Center(child: Text("222"));
+                        default:
+                          return Center(child: Text("予想外エラー"));
+                      }
+                    })));
+          } else {
 //请求未完成时弹出loading
-                  return Center(child: CircularProgressIndicator());
-                }
-              }),
-        ));
+            return Scaffold(
+                appBar: AppBar(
+                  //导航栏
+                  title: Text(widget.title),
+                ),
+                body: Center(child: CircularProgressIndicator()));
+          }
+        });
   }
 
   Widget _cat01ListView(ImmigrationStatisticsRoot rootModel) {
@@ -87,26 +106,30 @@ class _ImmigrationStatisticsPageState extends State<ImmigrationStatisticsPage> {
         .GET_STATS_DATA.STATISTICAL_DATA.CLASS_INF.CLASS_OBJ
         .firstWhere((e) => e.id == "cat01")
         .CLASS;
-    return Expanded(
-        child: ListView.separated(
-      padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
-      itemCount: CLASSList.length,
-      shrinkWrap: true,
-      itemBuilder: (BuildContext context, int index) => ListTile(
-        title: Text(CLASSList[index].name),
-        minVerticalPadding: 25,
-      ),
-      separatorBuilder: (BuildContext context, int index) => Divider(
-        height: 0.5,
-        indent: 20,
-        color: Colors.grey[120],
-      ),
-    ));
+    return ChangeNotifierProvider<ImmigrationStatisticsRoot>(
+        create: (_) => rootModel,
+        child: Builder(builder: (context) {
+          return Expanded(
+              child: ListView.separated(
+            padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+            itemCount: CLASSList.length,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) => ListTile(
+              title: Text(CLASSList[index].name),
+              minVerticalPadding: 25,
+            ),
+            separatorBuilder: (BuildContext context, int index) => Divider(
+              height: 0.5,
+              indent: 20,
+              color: Colors.grey[120],
+            ),
+          ));
+        }));
   }
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    model.selectedIndex = index;
+    // setState(() {
+    // });
   }
 }
