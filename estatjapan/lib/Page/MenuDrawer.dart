b37a312fe18d9b24d3fda849fda7ac00fbb20975
Enki <wdydhd@vip.qq.com>
2021-08-31 18:06:15 +0900

    ルールファイル作成

--- estatjapan/lib/Page/MenuDrawer.dart
+++ estatjapan/lib/Page/MenuDrawer.dart
@@ -19,19 +19,21 @@ class MenuDrawer extends StatelessWidget {
               padding: const EdgeInsets.only(top: 38.0),
               child: Row(
                 children: <Widget>[
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  //   child: ClipOval(
+                  //     child: Image().asset(
+                  //       "imgs/avatar.png",
+                  //       width: 80,
+                  //     ),
+                  //   ),
+                  // ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipOval(
-                      child: Image.asset(
-                        "imgs/avatar.png",
-                        width: 80,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "Wendux",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
+                      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                      child: Text(
+                        "在留資格取得等統計",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))
                 ],
               ),
             ),
