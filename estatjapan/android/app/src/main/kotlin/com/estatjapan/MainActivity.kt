3e75ffb67e0fc6557c89856a49d33d2325ede585
Enki <wdydhd@vip.qq.com>
2021-09-11 00:24:45 +0900

    テストコードコメントアウト

--- estatjapan/android/app/src/main/kotlin/com/estatjapan/MainActivity.kt
+++ estatjapan/android/app/src/main/kotlin/com/estatjapan/MainActivity.kt
@@ -9,16 +9,16 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity: FlutterActivity() {
 
     override fun onStart() {
-        print("bbbbbbb")
-        print(BuildConfig.DEBUG)
-        print(BuildConfig.BUILD_TYPE)
-        AlertDialog.Builder(this) // FragmentではActivityを取得して生成
-            .setTitle(BuildConfig.DEBUG.toString())
-            .setMessage(BuildConfig.AD_UNIT_ID)
-            .setPositiveButton("bOK", { dialog, which ->
-                // TODO:Yesが押された時の挙動
-            })
-            .show()
+//        print("bbbbbbb")
+//        print(BuildConfig.DEBUG)
+//        print(BuildConfig.BUILD_TYPE)
+//        AlertDialog.Builder(this) // FragmentではActivityを取得して生成
+//            .setTitle(BuildConfig.DEBUG.toString())
+//            .setMessage(BuildConfig.AD_UNIT_ID)
+//            .setPositiveButton("bOK", { dialog, which ->
+//                // TODO:Yesが押された時の挙動
+//            })
+//            .show()
         super.onStart()
     }
 }
