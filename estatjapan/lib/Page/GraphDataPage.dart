e8e01bc2fb00868e341eb2062138a86f4f26dc78
Enki <wdydhd@vip.qq.com>
2021-09-15 14:12:05 +0900

    グラフサンプリ作成

--- estatjapan/lib/Page/GraphDataPage.dart
+++ estatjapan/lib/Page/GraphDataPage.dart
@@ -1,4 +1,6 @@
+import 'package:estatjapan/Util/Indicator.dart';
 import 'package:estatjapan/model/GraphData.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class GraphDataPage extends StatefulWidget {
@@ -10,12 +12,9 @@ class GraphDataPage extends StatefulWidget {
 }
 
 class _GraphDataPageState extends State<GraphDataPage> {
+  int touchedIndex = -1;
   @override
   Widget build(BuildContext context) {
-    if (!widget.graphData.isModelExist()) {
-      Navigator.of(context).pop();
-      return const SizedBox();
-    }
     return Scaffold(
         appBar: AppBar(
           //导航栏
@@ -25,7 +24,149 @@ class _GraphDataPageState extends State<GraphDataPage> {
             // IconButton(icon: Icon(Icons.share), onPressed: () {}),
           ],
         ),
-        body: Text("aaaa"));
+        body: AspectRatio(
+          aspectRatio: 1,
+          child: Card(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: PieChart(
+                      PieChartData(
+                          pieTouchData: PieTouchData(touchCallback:
+                              (FlTouchEvent event, pieTouchResponse) {
+                            setState(() {
+                              if (!event.isInterestedForInteractions ||
+                                  pieTouchResponse == null ||
+                                  pieTouchResponse.touchedSection == null) {
+                                touchedIndex = -1;
+                                return;
+                              }
+                              touchedIndex = pieTouchResponse
+                                  .touchedSection!.touchedSectionIndex;
+                            });
+                          }),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          sectionsSpace: 0,
+                          centerSpaceRadius: 50,
+                          sections: showingSections()),
+                    ),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                        padding: EdgeInsets.only(
+                            right: MediaQuery.of(context).size.width * 0.1),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          // mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Indicator(
+                              color: Color(0xff0293ee),
+                              text: 'First',
+                              isSquare: true,
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Indicator(
+                              color: Color(0xfff8b250),
+                              text: 'Second',
+                              isSquare: true,
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Indicator(
+                              color: Color(0xff845bef),
+                              text: 'Third',
+                              isSquare: true,
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Indicator(
+                              color: Color(0xff13d38e),
+                              text: 'Fourth',
+                              isSquare: true,
+                            ),
+                            SizedBox(
+                              height: 18,
+                            ),
+                          ],
+                        ))),
+                const SizedBox(
+                  width: 28,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(4, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 17.0;
+      final radius = isTouched ? 110.0 : 90.0;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: const Color(0xff0293ee),
+            value: 40,
+            title: '40%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: const Color(0xfff8b250),
+            value: 30,
+            title: '30%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: const Color(0xff845bef),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 3:
+          return PieChartSectionData(
+            color: const Color(0xff13d38e),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          throw Error();
+      }
+    });
   }
 }
 
