dcf36dd627e0a884d1405716993b1a9fe68df1d9
Enki <wdydhd@vip.qq.com>
2021-09-27 16:45:48 +0900

    既済折れ線グラフ実装

--- estatjapan/lib/Page/LineGraphDataPage.dart
+++ estatjapan/lib/Page/LineGraphDataPage.dart
@@ -81,7 +81,7 @@ class _LineGraphDataPageState extends State<LineGraphDataPage> {
               Padding(
                 padding: const EdgeInsets.only(top: 15),
                 child: Text(
-                  "${widget.graphData.selectedCat01Mode!.name}の許可率",
+                  "${widget.graphData.selectedCat01Mode!.name}の毎月許可率",
                   textAlign: TextAlign.center,
                   style: const TextStyle(fontSize: 18),
                 ),
@@ -97,7 +97,7 @@ class _LineGraphDataPageState extends State<LineGraphDataPage> {
               Padding(
                 padding: const EdgeInsets.only(top: 20),
                 child: Text(
-                  "${widget.graphData.selectedCat01Mode!.name}の新規受理数",
+                  "${widget.graphData.selectedCat01Mode!.name}の毎月新規受理数",
                   textAlign: TextAlign.center,
                   style: const TextStyle(fontSize: 18),
                 ),
@@ -107,6 +107,22 @@ class _LineGraphDataPageState extends State<LineGraphDataPage> {
                 height: 360,
                 child: LineChart(
                   newApplicationData(rootModel),
+                  swapAnimationDuration: const Duration(milliseconds: 250),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  "${widget.graphData.selectedCat01Mode!.name}の毎月既済数",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 20, bottom: 110),
+                height: 360,
+                child: LineChart(
+                  argeedApplicationData(rootModel),
                   swapAnimationDuration: const Duration(milliseconds: 250),
                 ),
               )
@@ -321,6 +337,123 @@ class _LineGraphDataPageState extends State<LineGraphDataPage> {
                   .GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE
                   .where((element) =>
                       element.time == e.code && element.cat02 == "00102");
+              return FlSpot(
+                  timeModels.indexOf(e).toDouble(), value.first.valueDouble);
+            }).toList()),
+      ],
+      minX: 0,
+      maxX: timeModels.length.toDouble() - 1,
+      maxY: newApplicationMaxY,
+      minY: newApplicationMinY,
+    );
+  }
+
+  LineChartData argeedApplicationData(ImmigrationStatisticsRoot rootModel) {
+    final timeModels = rootModel
+        .GET_STATS_DATA.STATISTICAL_DATA.CLASS_INF.CLASS_OBJ
+        .firstWhere((element) => element.id == "time")
+        .CLASS
+        .toList();
+    timeModels.sort((left, right) => left.code.compareTo(right.code));
+    final newApplicationMaxY = () {
+      var maxY = 0.0;
+      for (var timeModel in timeModels) {
+        final values = rootModel.GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE
+            .where((element) =>
+                element.time == timeModel.code && element.cat02 == "00200")
+            .toList();
+        values.sort(
+            (left, right) => left.valueDouble.compareTo(right.valueDouble));
+        if (maxY < values.last.valueDouble) {
+          maxY = values.last.valueDouble;
+        }
+      }
+      return maxY;
+    }();
+
+    final newApplicationMinY = () {
+      var minY = double.infinity;
+      for (var timeModel in timeModels) {
+        final values = rootModel.GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE
+            .where((element) =>
+                element.time == timeModel.code && element.cat02 == "00200")
+            .toList();
+        values.sort(
+            (left, right) => left.valueDouble.compareTo(right.valueDouble));
+        if (minY > values.first.valueDouble) {
+          minY = values.first.valueDouble;
+        }
+      }
+      return minY * 0.96;
+    }();
+    return LineChartData(
+      lineTouchData: LineTouchData(enabled: true),
+      gridData: FlGridData(show: false),
+      titlesData: FlTitlesData(
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 50,
+          margin: 0,
+          interval: 1,
+          getTextStyles: (context, value) => const TextStyle(
+            color: Color(0xff72719b),
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          getTitles: (value) {
+            return timeModels[value.toInt()].name.characters.join("\n");
+          },
+        ),
+        rightTitles: SideTitles(showTitles: false),
+        topTitles: SideTitles(showTitles: false),
+        leftTitles: SideTitles(
+          getTitles: (value) {
+            final interval = newApplicationMaxY - newApplicationMinY;
+            if (value.toInt() == newApplicationMinY.toInt()) {
+              return value.toInt().toString();
+            } else if (value.toInt() == (newApplicationMaxY - 1).toInt()) {
+              return value.toInt().toString();
+            } else if (value.toInt() ==
+                ((interval / 2) + newApplicationMinY).toInt()) {
+              return value.toInt().toString();
+            } else {
+              return '';
+            }
+          },
+          showTitles: true,
+          margin: 0,
+          interval: 1,
+          reservedSize: 40,
+          getTextStyles: (context, value) => const TextStyle(
+            color: Color(0xff75729e),
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
+      ),
+      borderData: FlBorderData(
+        show: true,
+        border: const Border(
+          bottom: BorderSide(color: Color(0xff4e4965), width: 4),
+          left: BorderSide(color: Colors.transparent),
+          right: BorderSide(color: Colors.transparent),
+          top: BorderSide(color: Colors.transparent),
+        ),
+      ),
+      lineBarsData: [
+        LineChartBarData(
+            isCurved: true,
+            curveSmoothness: 0,
+            colors: const [Color(0xff4af699)],
+            barWidth: 4,
+            isStrokeCapRound: true,
+            dotData: FlDotData(show: false),
+            belowBarData: BarAreaData(show: false),
+            spots: timeModels.map((e) {
+              final value = rootModel
+                  .GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE
+                  .where((element) =>
+                      element.time == e.code && element.cat02 == "00200");
               return FlSpot(
                   timeModels.indexOf(e).toDouble(), value.first.valueDouble);
             }).toList()),
