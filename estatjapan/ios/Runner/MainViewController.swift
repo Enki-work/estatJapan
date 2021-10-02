d71a385db5dad4ee1b7d03aa713d89ab8a7b1c63
Enki <wdydhd@vip.qq.com>
2021-10-02 10:15:08 +0900

    Crashlytics対応する

---
+++ estatjapan/ios/Runner/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Runner
+//
+//  Created by YanQi on 2021/10/02.
+//
+
+import UIKit
+import Flutter
+
+class MainViewController: FlutterViewController {
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+}
